2a583c7c363b04a1dea12c56feab6aca50e1d48e
Luisfayre <luisfayre@hotmail.com>
2019-12-03 22:27:10 -0700

    Update ProfileFragment.java
    Detalles con FirebaseDatabase.getInstance().setPersistenceEnabled(true);

--- app/src/main/java/com/example/kipo/ProfileFragment.java
+++ app/src/main/java/com/example/kipo/ProfileFragment.java
@@ -14,20 +14,51 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.kipo.plants.plantDataItem;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
 
+    //Interface
     Button btnLogOut;
-
     View theInflatedView;
+    TextView txtUsername, txtUseremail;
+
+    //Firebase authentication
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private FirebaseUser user;
+
+    //Firebase database
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference myRef;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        theInflatedView = inflater.inflate(R.layout.fragment_profile,container,false);
+        theInflatedView = inflater.inflate(R.layout.fragment_profile, container, false);
 
         btnLogOut = theInflatedView.findViewById(R.id.btnLogOut);
+        txtUsername = theInflatedView.findViewById(R.id.txtUsername);
+        txtUseremail = theInflatedView.findViewById(R.id.txtUseremail);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+        //FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        //firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+
+        myRef.keepSynced(true);
+
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,10 +69,27 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userData value = dataSnapshot.getValue(userData.class);
+                txtUsername.setText("Username: " + value.getUsername());
+                txtUseremail.setText("Email: " + value.getEmail());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                //Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
         return theInflatedView;
     }
 
-    private void goLoginScreen(){
+    private void goLoginScreen() {
         //Se obtiene la actividad en la que se encuentra en fragmente actual y se destruye
         getActivity().finish();
 
