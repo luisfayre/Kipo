96b2de40203bc862f9dcb717a757a88e09d7f40e
Luisfayre <luisfayre@hotmail.com>
2019-12-03 22:27:34 -0700

    Datos de usuario

---
+++ app/src/main/java/com/example/kipo/userData.java
@@ -0,0 +1,31 @@
+package com.example.kipo;
+
+public class userData {
+    public String username;
+    public String email;
+
+    public userData() {
+
+    }
+
+    public userData(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
